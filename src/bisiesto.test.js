d7024608418c4fa1dabbb925f7e6ac823352c81c
Nicole Uribe <nikyaleuribe@hotmail.com>
2022-06-06 20:08:15 -0400

    deberoa devolver este año no es bosiesto para el año 5

--- src/bisiesto.test.js
+++ src/bisiesto.test.js
@@ -2,10 +2,10 @@ import añobi from "./bisiesto";
 
 describe("Saber si un año es bisiesto", () => {
     const  year= new añobi();
-  it("deberia devolver true para el año 4", () => {
+  it("deberia devolver este año es bisiesto para el año 4", () => {
     expect( year.bisiesto(4)).toEqual("este año es bisiesto");
   });
-  it("deberia devolver false para el año 5", () => {
+  it("deberia devolver este año no es bisiesto para el año 5", () => {
     expect( year.bisiesto(5)).toEqual("este año no es bisiesto");
-  });
+  })
 })
