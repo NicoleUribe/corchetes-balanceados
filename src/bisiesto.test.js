06bc4ab1a104bf2adbef3ad6dff07991401c964f
Nicole Uribe <nikyaleuribe@hotmail.com>
2022-06-06 20:10:47 -0400

    deberia devolver este año es bisiesto para el año 8

--- src/bisiesto.test.js
+++ src/bisiesto.test.js
@@ -8,4 +8,7 @@ describe("Saber si un año es bisiesto", () => {
   it("deberia devolver este año no es bisiesto para el año 5", () => {
     expect( year.bisiesto(5)).toEqual("este año no es bisiesto");
   })
+  it("deberia devolver este año es bisiesto para el año 8", () => {
+    expect( year.bisiesto(8)).toEqual("este año es bisiesto");
+  });
 })
